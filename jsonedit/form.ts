7ac5f078a95f702393ebb5d78446f0c8e7557303
marcelo a magalhaes <marcelo@maga.local>
2020-04-08 12:24:31 -0300

    jason edit improvements

--- jsonedit/form.ts
+++ jsonedit/form.ts
@@ -1,30 +1,19 @@
 import { app, get } from 'https://denopkg.com/syumai/dinatra/mod.ts';
 import { renderFile } from 'https://deno.land/x/dejs/mod.ts';
 const decoder = new TextDecoder('utf-8');
-const data_ = decoder.decode(await Deno.readFile('../content/data.json'));
 
-const data = JSON.parse(data_);
-
-console.log('-------------------------------------------------------')
-console.log('-------------------------------------------------------')
-console.log('-------------------------------------------------------')
-console.log('-------------------------------------------------------')
-console.log('-------------------------------------------------------')
 console.log('-------------------------------------------------------')
 
 let Mtree = {
   out: '',
-  pad: function (width: any, string: any, padding: any): any {
-    return (width <= string.length) ? string : this.pad(width, padding + string, padding)
-  },
   build: function (data: any, espaco = 0): any{
     this.out += '<ul>'
     const obj = Object.entries(data)
     for (const [key, value] of obj) {
       if (typeof value == 'object') {
-        this.out += '<li><a class=expand>' + this.pad(0, key, '.') + '</a>:'
+        this.out += '<li class=q><a class=expand>' + key + ':</a>'
       }else{
-        this.out += '<li><a class=expand>' + this.pad(10, key, '.') + '</a>:'
+        this.out += '<li><a class=item>' + key.padEnd(10,'.') + ':</a>'
       }
       // this.out += '<li>' + this.pad(10, key, '.') + ':'
        if (typeof value == 'object' && value != null){
@@ -33,7 +22,7 @@ let Mtree = {
          espaco--
        }else{
          espaco++
-         this.out += ' <input type=text value="' + value + '"></li>'
+         this.out += '<input style="margin-left: 2px;" type=text value="' + value + '"></li>'
          espaco--
        }
     }
@@ -42,24 +31,18 @@ let Mtree = {
   }
 }
 
-Mtree.build(data)
+
 
 app(
-  get('/',
+  get('/:dataFile',
     async ({ params }) => {
+      const data_ = decoder.decode(await Deno.readFile('../content/' + params.dataFile));
+      const data = JSON.parse(data_);
+      Mtree.out = ''
+      Mtree.build(data)
       return await renderFile('./tela.ejs', {
         tree: Mtree.out
       })
     }
   )
 )
-// console.log(typeof value)
-  // if (typeof value == 'object'){
-  //   const obj = Object.entries(data)
-  //   for (const [key, value] of obj) {
-  //     console.log(key,'--->', value)
-  //   }
-  // }
-
-
-// console.log(data);
