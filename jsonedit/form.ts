ad74703238ed5fec1b1eb0d83d2e0d75da00bb12
marcelo a magalhaes <marcelo@maga.local>
2020-04-09 16:20:10 -0300

    form extract

--- jsonedit/form.ts
+++ jsonedit/form.ts
@@ -1,35 +1,58 @@
 import { app, get } from 'https://denopkg.com/syumai/dinatra/mod.ts';
 import { renderFile } from 'https://deno.land/x/dejs/mod.ts';
 const decoder = new TextDecoder('utf-8');
-
+function arrayTest(y: any){
+  var ret = [] as any
+  y.forEach(function(o: any){
+    ret.push(o.split('.').map(function(x: any){if (x == +x) {return '[' + x + ']'} else {return x}}).join('.'))
+  })
+  return ret
+}
 console.log('-------------------------------------------------------')
 
 let Mtree = {
   out: '',
+  keys: [] as any,
+  ids: [] as any,
+  arrayTest: function arrayTest(y: any){
+    return  y.split('.').map(function(x: any){if (x == +x) {return '[' + x + ']'} else {return x}}).join('.').replace('.[', '[')
+  },
   build: function (data: any, espaco = 0): any{
+    var self = this
     this.out += '<ul>'
     const obj = Object.entries(data)
     for (const [key, value] of obj) {
       if (typeof value == 'object') {
-        this.out += '<li class=q><a class=expand>' + key + ':</a>'
+        this.out += '<li class=q><a class=expand>' + key + ':</a>\n'
       }else{
-        this.out += '<li><a class=item>' + key.padEnd(10,'.') + ':</a>'
+        this.out += '<li><a class=item>' + key.padEnd(10,'.') + ':</a>\n'
       }
       // this.out += '<li>' + this.pad(10, key, '.') + ':'
        if (typeof value == 'object' && value != null){
          espaco++
-          this.build(value, espaco)
+         this.keys.push(key);
+         this.build(value, espaco)
+         this.out += '</li>'
+         this.keys.pop()
          espaco--
        }else{
          espaco++
-         this.out += '<input style="margin-left: 2px;" type=text value="' + value + '"></li>'
+         this.keys.push(key);
+
+         var aa = this.arrayTest(this.keys.join('.'))
+
+
+         this.out += '<input id=' + aa + ' style="margin-left: 2px;" type=text value="' + value + '"></li>\n'
+         this.ids.push(this.keys.join('.'))
+         this.keys.pop()
          espaco--
        }
     }
-    this.out += '</ul>'
+    this.out += '</ul>\n'
   // return out
   }
 }
+
 
 
 
@@ -38,10 +61,14 @@ app(
     async ({ params }) => {
       const data_ = decoder.decode(await Deno.readFile('../content/' + params.dataFile));
       const data = JSON.parse(data_);
+      console.log(data);
+
       Mtree.out = ''
       Mtree.build(data)
       return await renderFile('./tela.ejs', {
-        tree: Mtree.out
+        data: JSON.stringify(data),
+        tree: Mtree.out,
+        ids: JSON.stringify(arrayTest(Mtree.ids))
       })
     }
   )
